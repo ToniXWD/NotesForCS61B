4a73b28a37155209bcb803780d35154ec1e7d67a
ToniXWD <807077266@qq.com>
2022-10-11 19:19:13 +0800

    "Update week3"

--- src/week3/List61B.java
+++ src/week3/List61B.java
@@ -13,14 +13,14 @@ public interface List61B<LochNess> {
 
     public LochNess removeLast();
 
-//    default allows to write the body of the method
-    default public void info(){
+    //    default allows to write the body of the method
+    default void info() {
         System.out.println("By XWD, xwdtoni@126.com");
     }
 
-    default public void print(){
-        for(int i=0; i<size();i++){
-            System.out.println(get(i) +" ");
+    default void print() {
+        for (int i = 0; i < size(); i++) {
+            System.out.println(get(i) + " ");
         }
         System.out.println();
     }
